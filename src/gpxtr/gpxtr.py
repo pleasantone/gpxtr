ca9b349801308ab7095567039b1d31b6d1a9edac
Paul Traina <pleasantone@users.noreply.github.com>
2023-08-28 16:18:15 -0700

    refactor and typecheck

--- src/gpxtr/gpxtr.py
+++ src/gpxtr/gpxtr.py
@@ -28,35 +28,33 @@ from shapely.geometry import Point
 KM_TO_MILES = 0.621371
 M_TO_FEET = 3.28084
 
-class GPXCalculator:
-    OUT_HDR = '|        Lat,Lon       | Name                           |   Dist. | G |  ETA  | Notes'
-    OUT_SEP = '| :------------------: | :----------------------------- | ------: | - | ----: | :----'
-    OUT_FMT = '| {:-10.4f},{:.4f} | {:30.30} | {:>7} | {:1} | {:>5} | {}{}'
+DEFAULT_TRAVEL_SPEED = 30.0 / KM_TO_MILES # 50kph or ~30mph
+DEFAULT_WAYPOINT_DELAYS = {
+    "Restaurant":   timedelta(minutes=60),
+    "Gas Station":  timedelta(minutes=15),
+    "Restroom":     timedelta(minutes=15),
+    "Photo":        timedelta(minutes=5)
+}
 
-    XML_NAMESPACE = {
-        'trp':  'http://www.garmin.com/xmlschemas/TripExtensions/v1',
-        'gpxx': 'http://www.garmin.com/xmlschemas/GpxExtensions/v3',
-    }
+OUT_HDR = '|        Lat,Lon       | Name                           |   Dist. | G |  ETA  | Notes'
+OUT_SEP = '| :------------------: | :----------------------------- | ------: | - | ----: | :----'
+OUT_FMT = '| {:-10.4f},{:.4f} | {:30.30} | {:>7} | {:1} | {:>5} | {}{}'
 
-    DEFAULT_TRAVEL_SPEED = 30.0 / KM_TO_MILES # 50kph or ~30mph
-    DEFAULT_WAYPOINT_DELAYS = {
-        "Restaurant":   timedelta(minutes=60),
-        "Gas Station":  timedelta(minutes=15),
-        "Restroom":     timedelta(minutes=15),
-        "Photo":        timedelta(minutes=5)
-    }
+XML_NAMESPACE = {
+    'trp':  'http://www.garmin.com/xmlschemas/TripExtensions/v1',
+    'gpxx': 'http://www.garmin.com/xmlschemas/GpxExtensions/v3',
+}
 
-    def __init__(self, gpx: GPX, imperial=True, speed=None, departure=None, waypoint_delays=None) -> None:
+class GPXTableCalculator:
+    def __init__(self, gpx: GPX, imperial:bool=True, speed: float=0.0,
+                 departure: Optional[datetime]=None, waypoint_delays: Optional[dict]=None) -> None:
         self.gpx = gpx
-        if speed:
-            self.speed =  speed / KM_TO_MILES if imperial else speed
-        else:
-            self.speed = self.DEFAULT_TRAVEL_SPEED
+        self.speed =  (speed / KM_TO_MILES if imperial else speed) or DEFAULT_TRAVEL_SPEED
         self.imperial = imperial
         self.depart_at = departure
-        self.waypoint_delays = waypoint_delays or self.DEFAULT_WAYPOINT_DELAYS
+        self.waypoint_delays = waypoint_delays or DEFAULT_WAYPOINT_DELAYS
 
-    def print_header(self, out=None) -> None:
+    def print_header(self, out: Optional[io.TextIOWrapper]=None) -> None:
         if self.gpx.name:
             print(f'# {self.gpx.name}', file=out)
         if self.gpx.creator:
@@ -70,16 +68,9 @@ class GPXCalculator:
         if self.speed:
             print(f'- Default speed: {self.format_speed(self.speed, True)}', file=out)
 
-    def symbol_delay(self, point: Union[GPXWaypoint, GPXRoutePoint]) -> timedelta:
-        return self.waypoint_delays.get(point.symbol or 'nil') or timedelta()
-
-    def travel_time(self, dist) -> timedelta:
-        """ distance is in meters, speed is in km/h """
-        return timedelta(minutes=dist / 1000.0 / self.speed * 60.0)
-
-    def print_waypoints(self, sort=None, out=None) -> None:
+    def print_waypoints(self, sort: str='', out: Optional[io.TextIOWrapper]=None) -> None:
         def _wpe() -> str:
-            return self.OUT_FMT.format(
+            return OUT_FMT.format(
                 point.geometry.x, point.geometry.y,
                 (point.name or '').replace('\n', ' '),
                 f'{self.format_long_length(round(point.track_distance - last_gas))}/{self.format_long_length(round(point.track_distance))}' if
@@ -90,68 +81,19 @@ class GPXCalculator:
                 point.symbol or '',
                 f' (+{str(self.symbol_delay(point))[:-3]})' if not first_point and self.symbol_delay(point) else '')
 
-        def _waypoint_from_point(point, name, symbol=None):
-            return GPXWaypoint(point.latitude, point.longitude, name=name, symbol=symbol)
-
-        def geodata_tracks(tracks: list[GPXTrack]) -> gpd.GeoDataFrame:
-            tracks_points = []
-            total_distance = 0.0
-            for track in tracks:
-                track_distance = 0.0
-                last = track.segments[0].points[0]
-                track_length = track.length_2d()
-                for segment in track.segments:
-                    for point in segment.points:
-                        delta = distance(last.latitude, last.longitude, None,
-                                         point.latitude, point.longitude, None)
-                        track_distance += delta
-                        total_distance += delta
-                        last = point
-                        tracks_points.append([Point(point.latitude, point.longitude),
-                                            track_distance, total_distance, track_length])
-            return gpd.GeoDataFrame(tracks_points,
-                                    columns=['geometry', 'track_distance', 'total_distance', 'track_length'],
-                                    crs="EPSG:4326") # type: ignore
-
-        def geodata_points(points: Union[list[GPXWaypoint], list[GPXRoutePoint]]) -> gpd.GeoDataFrame:
-            waypoints = []
-            for point in points:
-                waypoints.append([Point(point.latitude, point.longitude),
-                                point.name, point.symbol])
-            return gpd.GeoDataFrame(waypoints,
-                                    columns=['geometry', 'name', 'symbol'],
-                                    crs="EPSG:4326") # type: ignore
-
-        def geodata_nearest(points: gpd.GeoDataFrame, tracks: gpd.GeoDataFrame, sort=None) -> pd.DataFrame:
-            np_points = np.array(list(points.geometry.apply(lambda x: (x.x, x.y))))
-            np_tracks = np.array(list(tracks.geometry.apply(lambda x: (x.x, x.y))))
-            btree = cKDTree(np_tracks)
-            dist, idx = btree.query(np_points, k=1)
-            tracks_nearest = tracks.iloc[idx].drop(columns="geometry").reset_index(drop=True)
-            dataframe = pd.concat(
-                [
-                    points.reset_index(drop=True),
-                    tracks_nearest,
-                    pd.Series(dist, name='nearest')
-                ],
-                axis=1)
-            if sort:
-                dataframe = dataframe.sort_values(by=sort)
-            return dataframe
-
         for track in self.gpx.tracks:
     #       self.gpx.waypoints.append(_waypoint_from_point(track.segments[0].points[0],
     #            f'START: {track.name}', 'START'))
-            self.gpx.waypoints.append(_waypoint_from_point(track.segments[-1].points[-1],
+            self.gpx.waypoints.append(self.waypoint_from_point(track.segments[-1].points[-1],
                 f'END: {track.name}', 'END'))
         if self.gpx.waypoints and self.gpx.tracks:
             print('## Waypoints', file=out)
-            print(f'\n{self.OUT_HDR}\n{self.OUT_SEP}', file=out)
+            print(f'\n{OUT_HDR}\n{OUT_SEP}', file=out)
             last_gas = 0.0
             start_time = self.depart_at
             first_point = True
-            for point in geodata_nearest(geodata_points(self.gpx.waypoints),
-                                         geodata_tracks(self.gpx.tracks), sort=sort).itertuples():
+            for point in self.geodata_nearest(self.geodata_points(self.gpx.waypoints),
+                                              self.geodata_tracks(self.gpx.tracks), sort=sort).itertuples():
                 departure = start_time + self.travel_time(point.track_distance) if start_time else None
                 if last_gas > point.track_distance:   # assume we have filled up between segments
                     last_gas = 0.0
@@ -163,9 +105,9 @@ class GPXCalculator:
                     start_time += delay
                 first_point = False
 
-    def print_routes(self, out=None):
+    def print_routes(self, out: Optional[io.TextIOWrapper]=None) -> None:
         def _rpe() -> str:
-            return self.OUT_FMT.format(
+            return OUT_FMT.format(
                 point.latitude, point.longitude,
                 (point.name or '').replace('\n', ' '),
                 f'{self.format_long_length(dist - last_gas)}/{self.format_long_length(dist)}' if self.is_gas(point) or point is route.points[-1]
@@ -180,7 +122,7 @@ class GPXCalculator:
             if route.description:
                 print(f'- {route.description}', file=out)
             print(f'- {self.sun_rise_set(route.points[0])}', file=out)
-            print(f'\n{self.OUT_HDR}\n{self.OUT_SEP}', file=out)
+            print(f'\n{OUT_HDR}\n{OUT_SEP}', file=out)
             dist = 0.0
             previous = route.points[0].latitude, route.points[0].longitude
             last_gas = 0.0
@@ -205,11 +147,65 @@ class GPXCalculator:
                 current = point.latitude, point.longitude
                 dist += distance(previous[0], previous[1], None, current[0], current[1], None)
                 for extension in point.extensions:
-                    for extension_point in extension.findall('gpxx:rpt', self.XML_NAMESPACE):
+                    for extension_point in extension.findall('gpxx:rpt', XML_NAMESPACE):
                         current = float(extension_point.get('lat')), float(extension_point.get('lon'))
                         dist += distance(previous[0], previous[1], None, current[0], current[1], None)
                         previous = current
                 previous = current
+
+
+    @staticmethod
+    def waypoint_from_point(point, name: str, symbol: Optional[str]=None) -> GPXWaypoint:
+        return GPXWaypoint(point.latitude, point.longitude, name=name, symbol=symbol)
+
+    @staticmethod
+    def geodata_tracks(tracks: list[GPXTrack]) -> gpd.GeoDataFrame:
+        tracks_points = []
+        total_distance = 0.0
+        for track in tracks:
+            track_distance = 0.0
+            last = track.segments[0].points[0]
+            track_length = track.length_2d()
+            for segment in track.segments:
+                for point in segment.points:
+                    delta = distance(last.latitude, last.longitude, None,
+                                        point.latitude, point.longitude, None)
+                    track_distance += delta
+                    total_distance += delta
+                    last = point
+                    tracks_points.append([Point(point.latitude, point.longitude),
+                                        track_distance, total_distance, track_length])
+        return gpd.GeoDataFrame(tracks_points,
+                                columns=['geometry', 'track_distance', 'total_distance', 'track_length'],
+                                crs="EPSG:4326") # type: ignore
+
+    @staticmethod
+    def geodata_points(points: Union[list[GPXWaypoint], list[GPXRoutePoint]]) -> gpd.GeoDataFrame:
+        waypoints = []
+        for point in points:
+            waypoints.append([Point(point.latitude, point.longitude),
+                            point.name, point.symbol])
+        return gpd.GeoDataFrame(waypoints,
+                                columns=['geometry', 'name', 'symbol'],
+                                crs="EPSG:4326") # type: ignore
+
+    @staticmethod
+    def geodata_nearest(points: gpd.GeoDataFrame, tracks: gpd.GeoDataFrame, sort: str='') -> pd.DataFrame:
+        np_points = np.array(list(points.geometry.apply(lambda x: (x.x, x.y))))
+        np_tracks = np.array(list(tracks.geometry.apply(lambda x: (x.x, x.y))))
+        btree = cKDTree(np_tracks)
+        dist, idx = btree.query(np_points, k=1)
+        tracks_nearest = tracks.iloc[idx].drop(columns="geometry").reset_index(drop=True)
+        dataframe = pd.concat(
+            [
+                points.reset_index(drop=True),
+                tracks_nearest,
+                pd.Series(dist, name='nearest')
+            ],
+            axis=1)
+        if sort:
+            dataframe = dataframe.sort_values(by=sort)
+        return dataframe
 
     @staticmethod
     def format_time(time_s: float, seconds: bool) -> str:
@@ -239,19 +235,28 @@ class GPXCalculator:
             return f'{speed * KM_TO_MILES:.2f}{" mph" if units else ""}'
         return f'{speed:.2f}{" km/h" if units else ""}'
 
-    def layover(self, point: GPXRoutePoint) -> timedelta:
+    def symbol_delay(self, point: Union[GPXWaypoint, GPXRoutePoint]) -> timedelta:
+        return self.waypoint_delays.get(point.symbol or 'nil') or timedelta()
+
+    def travel_time(self, dist: float) -> timedelta:
+        """ distance is in meters, speed is in km/h """
+        return timedelta(minutes=dist / 1000.0 / self.speed * 60.0)
+
+    @staticmethod
+    def layover(point: GPXRoutePoint) -> timedelta:
         """ layover time at a given RoutePoint (Basecamp extension) """
         for extension in point.extensions:
-            for duration in extension.findall('trp:StopDuration', self.XML_NAMESPACE):
+            for duration in extension.findall('trp:StopDuration', XML_NAMESPACE):
                 match = re.match(r'^PT((\d+)H)?((\d+)M)?$', duration.text)
                 if match:
                     return timedelta(hours=int(match.group(2) or '0'), minutes=int(match.group(4) or '0'))
         return timedelta()
 
-    def departure_time(self, point: Union[GPXWaypoint, GPXRoutePoint]) -> Optional[datetime]:
+    @staticmethod
+    def departure_time(point: Union[GPXWaypoint, GPXRoutePoint]) -> Optional[datetime]:
         """ returns datetime object for route point with departure times or None """
         for extension in point.extensions:
-            for departure in extension.findall('trp:DepartureTime', self.XML_NAMESPACE):
+            for departure in extension.findall('trp:DepartureTime', XML_NAMESPACE):
                 return datetime.fromisoformat(departure.text.replace('Z', '+00:00'))
         return None
 
@@ -263,13 +268,13 @@ class GPXCalculator:
                 f'Sunset: {sun["sunset"].astimezone():%H:%M}')
 
     @staticmethod
-    def is_gas(point) -> str:
+    def is_gas(point: Union[GPXWaypoint, GPXRoutePoint]) -> str:
         if point.symbol and 'Gas Station' in point.symbol:
             return 'G'
         return ''
 
     @staticmethod
-    def shaping_point(point) -> bool:
+    def shaping_point(point: Union[GPXWaypoint, GPXRoutePoint]) -> bool:
         """ is a route point just a shaping/Via point? """
         if not point.name:
             return True
@@ -294,11 +299,12 @@ class DateParser(argparse.Action):
 def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("input", nargs="+", type=argparse.FileType('r'), help="input file(s)")
-    parser.add_argument("--html", action='store_true', help="output in HTML, not markdown")
     parser.add_argument("--output", type=argparse.FileType('w'), default=None, help="output file")
-    parser.add_argument("--sort", default=None, help="sort algorithm for waypoints (only)")
-    parser.add_argument("--departure", default=None, action=DateParser, help="set departure time for first point")
-    parser.add_argument("--speed", default=None, type=float, help="set average travel speed")
+    parser.add_argument("--sort", default="", type=str, help="sort algorithm (for waypoints only)")
+    parser.add_argument("--departure", default=None, action=DateParser, help="set departure time for first point (local timezone)")
+    parser.add_argument("--speed", default=0.0, type=float, help="set average travel speed")
+    parser.add_argument("--html", action='store_true', help="output in HTML, not markdown")
+    parser.add_argument("--metric", action='store_true', help="Use metric units (default imperial)")
 
     try:
         args = parser.parse_args()
@@ -317,7 +323,7 @@ def main() -> None:
     for handle in args.input:
         with handle as stream:
             try:
-                table = GPXCalculator(gpxpy.parse(stream), speed=args.speed, departure=args.departure)
+                table = GPXTableCalculator(gpxpy.parse(stream), speed=args.speed, departure=args.departure)
                 table.print_header(out=out)
                 table.print_waypoints(sort=args.sort, out=out)
                 table.print_routes(out=out)
