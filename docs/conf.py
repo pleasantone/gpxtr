cdeac9b299441034809037047ad1873908b6967f
Paul Traina <pleasantone@users.noreply.github.com>
2023-09-04 11:41:12 -0700

    don't import docs

--- docs/conf.py
+++ docs/conf.py
@@ -3,9 +3,6 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-import sys
-import os
-sys.path.insert(0, os.path.abspath('../src'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
